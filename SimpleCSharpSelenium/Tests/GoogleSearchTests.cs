10153631f5bb2793646362e7ee363b4d1404ea99
y3rsh <yersh13@gmail.com>
2014-01-06 13:15:47 -0600

    Initial commit of structure

---
+++ SimpleCSharpSelenium/Tests/GoogleSearchTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCSharpSelenium.Tests
+{
+    [TestClass]
+    public class GoogleSearchTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
