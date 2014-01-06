9ccea5f6bed7298c44619364b8b1ad1c030b806c
y3rsh <yersh13@gmail.com>
2014-01-06 16:37:18 -0600

    Little bit more

---
+++ SimpleCSharpSelenium/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpSelenium
+{
+    static class Constants
+    {
+        //TFS stuff this is the service account DO NOT ALTER
+        public const string TFS_URL = null;  //  "https://???.visualstudio.com/DefaultCollection";
+        public const string TFS_PROJECT_NAME = null;
+        public static string TFS_USER_NAME = null;
+        public static string TFS_USER_PASSWORD = null;
+        public static string TFS_DOMAIN = null; //remains null if using visualstudio.com
+    }
+}
---
+++ SimpleCSharpSelenium/Helper/TFSActions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.TestManagement.Client;
+using System.Net;
+using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SimpleCSharpSelenium.Helper
+{
+    static class TFSActions
+    {
+        /// <summary>
+        /// Get the parameters of the test case
+        /// </summary>
+        /// <param name="testCaseId">Test case id (work item id#) displayed into TFS</param>
+        /// <returns>Returns the test case parameters in datatable format. If there are no parameters then it will return null</returns>
+        public static DataTable GetTestCaseParameters(int testCaseId)
+        {
+            ITestManagementService TestMgrService;
+            ITestCase TestCase = null;
+            DataTable TestCaseParameters = null;
+
+            TfsTeamProjectCollection TeamProjectCollection
+                = new TfsTeamProjectCollection(new Uri(Constants.TFS_URL), new NetworkCredential(Constants.TFS_USER_NAME, Constants.TFS_USER_PASSWORD, Constants.TFS_DOMAIN));
+
+            TestMgrService = TeamProjectCollection.GetService<ITestManagementService>();
+            TestCase = TestMgrService.GetTeamProject(Constants.TFS_PROJECT_NAME).TestCases.Find(testCaseId);
+
+            if (TestCase != null)
+            {
+                if (TestCase.Data.Tables.Count > 0)
+                {
+                    TestCaseParameters = TestCase.Data.Tables[0];
+                }
+            }
+
+            return TestCaseParameters;
+        }
+    }
+}
